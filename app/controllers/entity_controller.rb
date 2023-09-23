60e3b5d3595b012a32fd2698b2dfbc21a6d5d197
camilevahviraki <camilevahviraki@gmail.com>
2023-09-23 02:48:38 +0200

    Update Entity controller

--- app/controllers/entity_controller.rb
+++ app/controllers/entity_controller.rb
@@ -1,16 +1,63 @@
 class EntityController < ApplicationController
    def index
+      render json: Entity.all
    end
    
    def show
+      entity = Entity.find(params[:id])
+      render json: entity
    end
    
    def create
+      name = params[:name]
+      location = params[:location]
+      description = params[:description]
+
+      entity = Entity.new(name:, location:, description:)
+
+      if entity.save
+         render json: { message: 'entity created successfully!' }
+       else
+         render json: { message: 'Error while saving entity!' }
+       end
    end
 
    def delete
+      entity = Entity.find(params[:id])
+      if entity.destroy
+         render json: { message: 'entity deleted successfully!' }
+       else
+         render json: { message: 'Error while deleting entity!' }
+       end
    end
    
    def update
+      id = params[:id]
+      data = params[:data]
+      field = params[:field]
+  
+      entity = Entity.find(id)
+
+      case field
+      when 'name'
+         if data.length > 2
+            entity.update(name: data)
+            render json: { message: 'name updated successfully!' }
+         else
+            render json: { message: 'The new name is too short!' }
+         end   
+      when 'description'
+         entity.update(name: data)
+         render json: { message: 'description updated successfully!' }
+      when 'location'
+         if data
+           entity.update(name: data)
+           render json: { message: 'location updated successfully!' }
+         else
+            render json: { message: 'Please! Provide a new location' }
+         end   
+      else
+         render json: { message: 'Error, Check your params data!' }
+      end
    end 
 end    
